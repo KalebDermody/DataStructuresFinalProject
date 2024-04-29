d8c423615b74ac142ad67774711e7a7aebcf6659
KalebDermody <kaleb.derm@gmail.com>
2024-04-29 16:21:57 -0500

    1

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-from tkinter import filedialog, messagebox
+from tkinter import Frame, messagebox, PhotoImage, Label, Button, Toplevel
 
 
 class RecipeLinkedList:
@@ -27,31 +27,38 @@ class RecipeApplication:
     def __init__(self, master):
         self.master = master
         self.master.title("Recipie Book")
-        self.master.geometry("720x300")
-        self.gui()
-
-    def gui(self):
-        self.AddButton = tk.Button(self.master, text="add recipe", command=self.AddRecipe)
-        self.AddButton.pack()
-
-        self.SearchButton = tk.Button(self.master, text="Search", command=self.SearchRecipe)
-        self.SearchButton.pack()
-
-        self.ViewButton = tk.Button(self.master, text="View List", command=self.ViewList)
-        self.ViewButton.pack()
+        self.master.geometry("620x680")
+        self.bg = PhotoImage(file="gusteaus.png")
+        # Show image using label
+        label1 = Label(master, image=self.bg)
+        label1.place(x=0, y=0)
+        label2 = Label(master, text="Welcome")
+        label2.pack(pady=50)
+        # Create Frame
+        frame1 = Frame(master)
+        frame1.pack(pady=20)
+        # Add buttons
+        self.AddButton = Button(self.master, text="add recipe", command=self.AddRecipe)
+        self.AddButton.pack(pady=20)
+        self.SearchButton = Button(self.master, text="Search", command=self.SearchRecipe)
+        self.SearchButton.pack(pady=20)
+        self.ViewButton = Button(self.master, text="View List", command=self.ViewList)
+        self.ViewButton.pack(pady=20)
+        self.exit_button = Button(master, text="Exit", command=master.destroy)
+        self.exit_button.pack(pady=20)
 
     def AddRecipe(self):
-        addWindow = tk.Toplevel(self.master)
+        addWindow = Toplevel(self.master)
         addWindow.title("Add New Recipe")
         addWindow.geometry("400x300")
 
     def SearchRecipe(self):
-        addWindow = tk.Toplevel(self.master)
+        addWindow = Toplevel(self.master)
         addWindow.title("Search For Recipe")
         addWindow.geometry("400x300")
 
     def ViewList(self):
-        addWindow = tk.Toplevel(self.master)
+        addWindow = Toplevel(self.master)
         addWindow.title("View sorted full list of recipes")
         addWindow.geometry("400x300")
 
